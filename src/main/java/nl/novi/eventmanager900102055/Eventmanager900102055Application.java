435e5d6d55fe9145e6bd8497f373d5d2fad785f7
stevenminken <stevenminken@gmail.com>
2023-06-29 12:26:53 +0200

    initial commit

---
+++ src/main/java/nl/novi/eventmanager900102055/Eventmanager900102055Application.java
@@ -0,0 +1,13 @@
+package nl.novi.eventmanager900102055;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eventmanager900102055Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eventmanager900102055Application.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/eventmanager900102055/Eventmanager900102055ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.eventmanager900102055;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eventmanager900102055ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
